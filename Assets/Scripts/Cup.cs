5f4897e764a61acb0da6f36359e89c61a2c92248
AndrewForce2 <andrei3edw@gmail.com>
2021-05-23 23:29:01 +0300

    Cups and Balls

---
+++ Assets/Scripts/Cup.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+    public float downHeight = 0.75f;
+    public float upHeight = 1f;
+    public float movingSpeed = 0.3f;
+
+    public GameObject ball;
+    public Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+        downHeight = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (started_option == true)
+        //{
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * movingSpeed);
+            
+            if (ball != null)
+            {
+            ball.transform.position = new Vector3(
+                    transform.position.x,
+                    ball.transform.position.y,
+                    transform.position.z
+                );
+            }
+        //    if (Vector3.Distance(transform.position, targetPosition) < 1)
+        //        started_option = false;
+        //}
+        //Debug.Log(targetPosition.y);
+    }
+
+    public void MoveUp ()
+    {
+        targetPosition = new Vector3(
+                transform.position.x,
+                upHeight,
+                transform.position.z
+            );
+    }
+
+    public void MoveDown()
+    {
+        targetPosition = new Vector3(
+                transform.position.x,
+                downHeight,
+                transform.position.z
+            );
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool canPick = false;
+
+    public bool picked = false;
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        transform.position = new Vector3(
+                0,
+                0,
+                2.476f
+            );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (canPick)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit) && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Debug.Log(hit.transform.name);
+                Cup cup = hit.transform.GetComponent<Cup>();
+                if (cup != null)
+                {
+                    canPick = false;
+
+                    picked = true;
+                    won = (cup.ball != null);
+                    cup.MoveUp();
+                }
+            }
+        }
+    }
+}
