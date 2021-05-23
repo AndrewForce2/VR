dd6102a2bd6382bd2856765b0de7f003b0f1ea6b
AndrewForce2 <andrei3edw@gmail.com>
2021-05-23 23:35:36 +0300

    Escape

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Player player;
+    public GameObject door;
+    public TextMesh gameText;
+    public GameObject itemContainer;
+    public float itemSize = 0.45f;
+
+    private Vector3 originalDoorPosition;
+    private float gameTimer;
+    private float gameOverTimer = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalDoorPosition = door.transform.position;
+
+        GameObject randomItem = itemContainer.transform.GetChild(Random.Range(0, itemContainer.transform.childCount)).gameObject;
+        randomItem.transform.localScale += Vector3.one * itemSize;
+        randomItem.tag = "CorrectItem";
+        Debug.Log(randomItem.transform.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.hasKey)
+        {
+            door.transform.position = Vector3.Lerp(
+                    door.transform.position,
+                    originalDoorPosition + new Vector3(0, 10f, 0),
+                    Time.deltaTime
+                ); ;
+
+            gameText.text = "You escaped :D\nTime: " + Mathf.FloorToInt(gameTimer);
+
+            gameOverTimer -= Time.deltaTime;
+            if(gameOverTimer <= 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else
+        {
+            gameTimer += Time.deltaTime;
+
+            gameText.text = "Find the key!\nTime: " + Mathf.FloorToInt(gameTimer);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController characterController;
+    public float gravity = -9.81f;
+    public float MovementSpeed = 4f;
+    public bool hasKey = false;
+
+    private Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        // player movement - forward, backward, left, right
+        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
+        characterController.Move((Camera.main.transform.right * horizontal + Camera.main.transform.forward * vertical) * Time.deltaTime);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if(hit.transform.tag == "CorrectItem")
+                {
+                    hasKey = true;
+
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
