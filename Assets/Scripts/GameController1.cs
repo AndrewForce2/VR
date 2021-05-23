de91ad698b213e7db58b8dce0dd5d5445fd8c945
AndrewForce2 <andrei3edw@gmail.com>
2021-05-23 22:54:52 +0300

    Bowling

---
+++ Assets/Scripts/GameController1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController1 : MonoBehaviour
+{
+    public Player player;
+    public TextMesh infoText;
+    public Pin[] pins;
+
+    public float evaluationTime = 10f;
+
+    private float gameTimer = 0f;
+    private bool evaluating = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        infoText.text = "Throw the bowling ball!" + '\n' + "Timer: " + Mathf.Floor(gameTimer);
+
+        if(evaluating == false)
+        {
+            if(player.holding == false)
+            {
+                evaluating = true;
+                gameTimer = evaluationTime;
+            }
+        }
+        else
+        {
+            gameTimer -= Time.deltaTime;
+            if (gameTimer <= 0f)
+            {
+                int score = 0;
+                for (int i = 0; i < pins.Length; i++)
+                {
+                    if (pins[i] == null)
+                    {
+                        score++;
+                    }
+                }
+                if(score == 10)
+                {
+                    infoText.text = "STRIKE";
+                }
+                else infoText.text = "Your score: " + score;
+            }
+
+            if (gameTimer <= -3f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bowlingBall;
+    public float ballDistance = 2.25f;
+    public float ballThrowingForce = 200f;
+
+    public bool holding = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(holding)
+        {
+            bowlingBall.transform.position = Camera.main.transform.position + Camera.main.transform.forward * ballDistance;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                holding = false;
+            }
+        }
+        else
+        {
+            bowlingBall.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * ballThrowingForce);
+            
+        }
+       
+    }
+}
