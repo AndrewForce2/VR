4d711f968b3346770625208fd99b84545e754077
AndrewForce2 <andrei3edw@gmail.com>
2021-05-24 12:47:42 +0300

    Motor

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public TextMesh infoText;
+    public GameObject[] obstaclePrefabs;
+    public Player player;
+    public GameObject finishLine;
+
+    public float spawnDistanceFromPlayer = 20f;
+    public float spawnDistanceFromObstacles = 5f;
+    public float finishLinePosition = 200f;
+
+    private AudioSource audio;
+    private float obstaclePointer;
+    private float gameTimer;
+    private float gameOverTimer = 3f;
+    private bool isGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+        finishLine.transform.position = new Vector3(
+                finishLine.transform.position.x,
+                finishLine.transform.position.y,
+                finishLinePosition
+            ) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(obstaclePointer < player.transform.position.z)
+        {
+            obstaclePointer += spawnDistanceFromObstacles;
+
+            GameObject obstacleObject = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)]);
+            obstacleObject.transform.position = new Vector3(
+                    Random.Range(-4f, 4f),
+                    obstacleObject.transform.position.y,
+                    player.transform.position.z + spawnDistanceFromPlayer
+                ) ;
+            obstacleObject.transform.eulerAngles = new Vector3(
+                    0,
+                    Random.Range(-30f, 30f),
+                    0
+                );
+        }
+
+        
+
+        if(!isGameOver) 
+        {
+            if (player.hasReachedFinishLine)
+            {
+                isGameOver = true;
+            }
+            gameTimer += Time.deltaTime;
+            infoText.text = "Time: " + Mathf.FloorToInt(gameTimer);
+
+        } else
+        {
+            infoText.text = "Game over!\nYour time: " + Mathf.FloorToInt(gameTimer) ;
+
+            gameOverTimer -= Time.deltaTime;
+            if(gameOverTimer <= 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject motorcycle;
+    public float acceleration = 0.5f;
+    public float maximumSpeed = 10f;
+    public float obstacleSlowdown = 0.25f;
+    public bool hasReachedFinishLine;
+
+    public float speed = 2f;
+    private bool isGameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Acceleration logic
+
+        speed += acceleration * Time.deltaTime;
+        if(speed > maximumSpeed)
+        {
+            speed = maximumSpeed;
+        }
+
+        Vector3 direction = new Vector3(
+                Camera.main.transform.forward.x,
+                0,
+                Camera.main.transform.forward.z
+            );
+
+        if (Camera.main.transform.forward.z < 0)
+        {
+            direction = Vector3.zero;
+        }
+        transform.position += direction.normalized * speed * Time.deltaTime;
+
+        if(transform.position.x < -5f)
+        {
+            transform.position = new Vector3(-5f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 5f)
+        {
+            transform.position = new Vector3(5f, transform.position.y, transform.position.z);
+        }
+
+        motorcycle.transform.eulerAngles = new Vector3(
+                motorcycle.transform.eulerAngles.x,
+                Camera.main.transform.eulerAngles.y + 90f,
+                motorcycle.transform.eulerAngles.z
+            );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            speed -= 2.4f;
+        }
+        else if(other.tag == "FinishLine")
+        {
+            hasReachedFinishLine = true;
+        }
+    }
+}
